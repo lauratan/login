38f7c22338d75bb78ae10cfc8f36a55ff3514907
Laura Tan <laura.tan108@gmail.com>
2019-08-29 14:17:38 -0700

    Removed not needed files and set up project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useReducer} from 'react';
+import Reducer      from './context/reducer';
+import {
+  LOGIN,
+  TEST
+}                   from './context/types';
+import Context from './context/context.js';
+import {BrowserRouter, Route} from 'react-router-dom';
+import Login from './components/Login';
+import Test from './components/Test';
 
-function App() {
+
+const App = () => {
+  const initialState = {
+    loggedIn: false,
+    user: {}
+  }
+  const [state, dispatch] = useReducer(Reducer, initialState);
+  
+  const login = (credentials) => {
+    dispatch({
+      type: LOGIN,
+      payload: credentials
+    })
+  }
+
+  const testLog = (arg) => {
+    dispatch({
+      type: TEST,
+      payload: arg
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Context.Provider
+      value={{
+        loggedIn: state.loggedIn,
+        user: state.user,
+        login,
+        testLog
+      }}
+    >
+       <BrowserRouter>
+        <div id="router">
+          <Route exact path="/" component={Login}/>
+          <Route exact path="/test" component={Test}/>
+        </div>
+      </BrowserRouter>
+    </Context.Provider>
   );
 }
 
